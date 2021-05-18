9121f0f2dae8de9e666e725fff450bdde8afebc7
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2021-05-18 16:12:51 -0500

    Avitab, Partial Fix #296

--- plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.eicasctl.lua
+++ plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.eicasctl.lua
@@ -1,4 +1,4 @@
-registerFMCCommand("laminar/B747/dsp/eng_switch","")
+--[[registerFMCCommand("laminar/B747/dsp/eng_switch","")
 registerFMCCommand("laminar/B747/dsp/stat_switch","")
 registerFMCCommand("laminar/B747/dsp/canc_switch","")
 registerFMCCommand("laminar/B747/dsp/fuel_switch","")
@@ -7,7 +7,7 @@ registerFMCCommand("laminar/B747/dsp/rcl_switch","")
 registerFMCCommand("laminar/B747/dsp/elec_switch","")
 registerFMCCommand("laminar/B747/dsp/ecs_switch","")
 registerFMCCommand("laminar/B747/dsp/hyd_switch","")
-registerFMCCommand("laminar/B747/dsp/drs_switch","")
+registerFMCCommand("laminar/B747/dsp/drs_switch","")]]
 fmsPages["EICASMODES"]=createPage("EICASMODES")
 fmsPages["EICASMODES"]["template"]={
 
@@ -17,7 +17,7 @@ fmsPages["EICASMODES"]["template"]={
 "                        ",
 "<STAT              GEAR>",
 "                        ",
-"                        ",
+"<AVITAB                 ",
 "                        ",
 "                        ", 
 "------------------------",
@@ -29,13 +29,15 @@ fmsPages["EICASMODES"]["template"]={
 
 fmsFunctionsDefs["EICASMODES"]={}
 
-fmsFunctionsDefs["EICASMODES"]["L1"]={"doCMD","laminar/B747/dsp/eng_switch"}
-fmsFunctionsDefs["EICASMODES"]["L2"]={"doCMD","laminar/B747/dsp/stat_switch"}
+fmsFunctionsDefs["EICASMODES"]["L1"]={"setDref","eicas_eng"}
+fmsFunctionsDefs["EICASMODES"]["L2"]={"setDref","eicas_stat"}
+fmsFunctionsDefs["EICASMODES"]["L3"]={"setDref","eicas_avitab"}
 fmsFunctionsDefs["EICASMODES"]["L5"]={"doCMD","laminar/B747/dsp/canc_switch"}
-fmsFunctionsDefs["EICASMODES"]["R1"]={"doCMD","laminar/B747/dsp/fuel_switch"}
-fmsFunctionsDefs["EICASMODES"]["R2"]={"doCMD","laminar/B747/dsp/gear_switch"}
+fmsFunctionsDefs["EICASMODES"]["R1"]={"setDref","eicas_fuel"}
+fmsFunctionsDefs["EICASMODES"]["R2"]={"setDref","eicas_gear}
 fmsFunctionsDefs["EICASMODES"]["R5"]={"doCMD","laminar/B747/dsp/rcl_switch"}
 fmsFunctionsDefs["EICASMODES"]["R6"]={"setpage","EICASSYN"}
+
 
 fmsPages["EICASSYN"]=createPage("EICASSYN")
 fmsPages["EICASSYN"]["template"]={
@@ -58,10 +60,10 @@ fmsPages["EICASSYN"]["template"]={
 
 fmsFunctionsDefs["EICASSYN"]={}
 
-fmsFunctionsDefs["EICASSYN"]["L1"]={"doCMD","laminar/B747/dsp/elec_switch"}
-fmsFunctionsDefs["EICASSYN"]["L2"]={"doCMD","laminar/B747/dsp/ecs_switch"}
+fmsFunctionsDefs["EICASSYN"]["L1"]={"setDref","eicas_elec"}
+fmsFunctionsDefs["EICASSYN"]["L2"]={"setDref","eicas_ecs"}
 fmsFunctionsDefs["EICASSYN"]["L5"]={"doCMD","laminar/B747/dsp/canc_switch"}
-fmsFunctionsDefs["EICASSYN"]["R1"]={"doCMD","laminar/B747/dsp/hyd_switch"}
-fmsFunctionsDefs["EICASSYN"]["R2"]={"doCMD","laminar/B747/dsp/drs_switch"}
+fmsFunctionsDefs["EICASSYN"]["R1"]={"setDref","eicas_hyd"}
+fmsFunctionsDefs["EICASSYN"]["R2"]={"setDref","eicas_drs"}
 fmsFunctionsDefs["EICASSYN"]["R5"]={"doCMD","laminar/B747/dsp/rcl_switch"}
 fmsFunctionsDefs["EICASSYN"]["R6"]={"setpage","EICASMODES"}
