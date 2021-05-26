cfcfe0a7443b20ca3f69d71587a7002d015dff6b
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2021-05-26 15:26:49 -0500

    READ THE README

---
+++ plugins/xtlua/scripts/B747.68.xt.fms/activepages/readme.lua
@@ -0,0 +1,34 @@
+fmsPages["README"]=createPage("README")
+fmsPages["README"].getPage=function(self,pgNo,fmsID)  
+    return {
+      "    Read The Readme!    ",
+      "                        ",
+      "THANKS FOR DOWNLOADING  ",
+      "THE SPARKY744! TO UNLOCK",
+      "FMC, PLEASE ENTER PIN   ",
+      "FOUND IN THE README!    ",
+      "
+      "PIN:                    ",
+      "____                    ",
+      "                        ",
+      "      "..lineA,
+      "                        ",
+      lineB
+    }
+    
+    fmsFunctionsDefs["README"]["L3"]={"setDref","readmePIN"}
+  
+    if B747DR_readMe_read == 1 then -- unlocked
+    fmsFunctionsDefs["README"]["L6"]={"setpage","MENU"}
+    lineA = "CORRECT!"
+    lineB = "<RETURN"
+    elseif B747DR_readMe_read == 2 -- failed
+    fmsFunctionsDefs["README"]["L6"]=nil
+    lineA = "INVALID PIN"
+    lineB = ""
+    else -- not entered
+    fmsFunctionsDefs["README"]["L6"]=nil
+    lineA = ""
+    lineB = ""
+    end
+end
